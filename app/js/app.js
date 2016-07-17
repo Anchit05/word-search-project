fbfdee7b1630099e16047c7d072189c82a7235ee
Anchit <anchit@Anchits-MacBook-Pro.local>
2016-07-17 15:04:25 +0530

    added UI code for uploading

--- app/js/app.js
+++ app/js/app.js
@@ -1,3 +1,88 @@
+var d = document,
+finalData = [];
+
+d.getElementById("uploadBtn").onchange = function () {
+	d.getElementById("uploadFile").value = this.value;
+	var fr = new FileReader();
+    console.log("file Obj: ", fr);
+    fr.onload = function() {
+        var ifEmpty = $("input[type='file']").filter(function (){
+            return !this.value
+        }).length;
+        if (!ifEmpty && !this.result.trim().length) {   //to check if file is empty or have only spaces
+            alert("File is empty");
+        } else {
+            var arr = [];
+            arr = this.result.replace( /\n/g, " " );
+            arr = arr.replace(/[!@#$%^&*,.:>{<}()]/g, "" ).split( " " ); //to ignore special characters in file
+            console.log("array: ", arr);
+            //d.getElementById("fileContent").textContent = this.result;
+            console.log("this.result: ", this.result);
+            var ignoreWords = wordsToIgnore();
+            finalData = checkCount(arr,ignoreWords);
+           // console.log("data:", checkCount(arr,ignoreWords));
+        }
+    }
+    fr.readAsText(this.files[0]);
+	if (this.value) {
+		$('.report-option').prop("disabled", false);
+		$('#gen_report_btn-id').prop("disabled", false);
+	}
+};
+
+function clearUpload () {
+	d.getElementById("uploadFile").value = "";
+	$('.report-option').prop("disabled", true);
+	$('#gen_report_btn-id').prop("disabled", true);
+};
+
+function generateReport() {
+	var chartType = $('.report-option')[0].innerText,
+	data;
+	console.log("chartType", chartType);
+	chartType = chartType.trim();
+	if (chartType === "Pie Chart") {
+		$('#pie_chart').css("display", "block");
+		$('#bar_chart').css("display", "none");
+		$('#visual_chart').css("display", "none");
+		drawBarChart(data);
+	} else if (chartType === "Bar Chart") {
+		$('#bar_chart').css("display", "block");
+		$('#pie_chart').css("display", "none");
+		$('#visual_chart').css("display", "none");
+		drawPieChart(data);
+	} else {
+		$('#visual_report').css("display", "block");
+		$('#pie_chart').css("display", "none");
+		$('#bar_chart').css("display", "none");
+		drawVisualReport(data);
+	}
+};
+
+function showHideCharts() {
+
+}
+
+$(".dropdown-menu li a").click(function(){
+	var target = $(this).html();
+	$(this).parents('.dropdown-menu').find('li').removeClass('active');
+    $(this).parent('li').addClass('active');
+   	$(this).parents('#generate_report_id').find('.dropdown-toggle')
+   		.html(target + ' <span class="caret"></span>');
+});
+
+function drawBarChart(data) {
+	console.log("bar chart");
+};
+
+function drawPieChart(data) {
+	console.log("pie chart");
+};
+
+function drawVisualReport(data) {
+	console.log("visual chart");
+};
+
 function Trie() {
     this.words = 0;
     this.prefixes = 0;
@@ -89,26 +174,6 @@ function checkCount(arr,ignoreWords) {
     }
 //    console.log("Map of words: ",maxFrequencyMap);
     sortedWords = sortFrequencyMap(maxFrequencyMap);
+    return sortedWords;
 //    console.log("sorted of words: ",sortedWords);
 };
-document.getElementById("openfile").addEventListener('change',function(){
-    var fr = new FileReader();
-    console.log("file Obj: ", fr);
-    fr.onload = function() {
-        var ifEmpty = $("input[type='file']").filter(function (){
-            return !this.value
-        }).length;
-        if (!ifEmpty && !this.result.trim().length) {   //to check if file is empty or have only spaces
-            alert("File is empty");
-        } else {
-            var arr = [];
-            arr = this.result.replace( /\n/g, " " );
-            arr = arr.replace(/[!@#$%^&*,.:]/g, "" ).split( " " ); //to ignore special characters in file
-            console.log("array: ", arr);
-            document.getElementById("fileContent").textContent = this.result;
-            var ignoreWords = wordsToIgnore();
-            checkCount(arr,ignoreWords);
-        }
-    }
-    fr.readAsText(this.files[0]);
-});
